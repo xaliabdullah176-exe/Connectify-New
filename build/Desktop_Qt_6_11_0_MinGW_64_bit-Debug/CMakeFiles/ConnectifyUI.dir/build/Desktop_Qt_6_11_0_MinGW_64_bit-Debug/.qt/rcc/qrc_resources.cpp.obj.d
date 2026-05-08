5f2ae82a83e608b9d176479c493090d0a50890d2
farzamzeeshan1 <farzamzeeshan1@gmail.com>
2026-05-09 01:22:30 +0500

    Delete build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/ConnectifyUI.dir/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/rcc/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/ConnectifyUI.dir/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/rcc/qrc_resources.cpp.obj: \
- C:\Users\laptech\Downloads\Connectify\ (1)\Connectify\build\Desktop_Qt_6_11_0_MinGW_64_bit-Debug\.qt\rcc\qrc_resources.cpp
